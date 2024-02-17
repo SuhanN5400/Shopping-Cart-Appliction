914d6c2d846e01bb37f4d7c202660688045e1688
SUHAN N <151826206+SuhanN5400@users.noreply.github.com>
2024-02-17 22:02:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,94 @@
+//App.js
+
+import React, { useState } from 'react';
+import './App.css';
+import SearchComponent from './components/SearchComponent';
+import ShowCourseComponent from './components/ShowCourseComponent';
+import UserCartComponent from './components/UserCartComponent';
+
+function App() {
+	const [courses, setCourses] = useState([
+		{ id: 1, 
+		name: 'Chocolate', 
+		price: 2999, 
+		image: 
+'https://chocol8.co.uk/wp-content/uploads/2022/08/order-online-choose-your-own-chocolates.jpg'
+		},
+		{ id: 2, 
+		name: 'Gucci Bag', 
+		price: 9999, 
+		image: 
+'https://luxurypromise.com/cdn/shop/files/gucci-handbags-gucci-gg-supreme-web-jackie-1961-large-tote-36130636300444.jpg?v=1685550853'
+		},
+		{ id: 3, 
+		name: 'Parker Fountain Pen', 
+		price: 7999, 
+		image: 
+'https://s7d9.scene7.com/is/image//NewellRubbermaid/2190512_im_imgps_20-v1'
+		}
+	]);
+
+	const [cartCourses, setCartCourses] = useState([]);
+	const [searchCourse, setSearchCourse] = useState('');
+
+	const addCourseToCartFunction = (GFGcourse) => {
+		const alreadyCourses = cartCourses
+							.find(item => item.product.id === GFGcourse.id);
+		if (alreadyCourses) {
+			const latestCartUpdate = cartCourses.map(item =>
+				item.product.id === GFGcourse.id ? { 
+				...item, quantity: item.quantity + 1 } 
+				: item
+			);
+			setCartCourses(latestCartUpdate);
+		} else {
+			setCartCourses([...cartCourses, {product: GFGcourse, quantity: 1}]);
+		}
+	};
+
+	const deleteCourseFromCartFunction = (GFGCourse) => {
+		const updatedCart = cartCourses
+							.filter(item => item.product.id !== GFGCourse.id);
+		setCartCourses(updatedCart);
+	};
+
+	const totalAmountCalculationFunction = () => {
+		return cartCourses
+			.reduce((total, item) => 
+						total + item.product.price * item.quantity, 0);
+	};
+
+	const courseSearchUserFunction = (event) => {
+		setSearchCourse(event.target.value);
+	};
+
+	const filterCourseFunction = courses.filter((course) =>
+		course.name.toLowerCase().includes(searchCourse.toLowerCase())
+	);
+
+	return (
+		<div className="App">
+			<SearchComponent searchCourse={searchCourse} 
+							courseSearchUserFunction=
+								{courseSearchUserFunction} />
+			<main className="App-main">
+				<ShowCourseComponent
+					courses={courses}
+					filterCourseFunction={filterCourseFunction}
+					addCourseToCartFunction={addCourseToCartFunction}
+				/>
+
+				<UserCartComponent
+					cartCourses={cartCourses}
+					deleteCourseFromCartFunction={deleteCourseFromCartFunction}
+					totalAmountCalculationFunction={
+						totalAmountCalculationFunction
+					}
+					setCartCourses={setCartCourses}
+				/>
+			</main>
+		</div>
+	);
+}
+
+export default App;
